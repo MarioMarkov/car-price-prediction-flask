b4d56efd80e763843d5132e6e30388c295a83747
Mario Markov <mario11septemvri@gmail.com>
2023-02-05 21:55:12 +0200

    cleaned up code 🗑️

--- static/js/get_models_ajax.js
+++ static/js/get_models_ajax.js
@@ -4,6 +4,26 @@ var e = document.getElementById("brand");
 var value = e.value;
 var val = e.options[e.selectedIndex].text;
 var xhr = new XMLHttpRequest();
+
+
+function removeOptions(selectElement) {
+  var i, L = selectElement.options.length - 1;
+  for(i = L; i >= 0; i--) {
+      selectElement.remove(i);
+  }
+}
+
+function add_new_options(carModels,select) {
+  //Loop through the car models and add them as options to the select element
+  for (var i = 0; i < carModels.length; i++) {
+    var option = document.createElement("option");
+    option.text = carModels[i];
+    
+    select.add(option);
+  }
+
+}
+
 xhr.open("GET", `http://127.0.0.1:5000/get_brands/${val}`);
 xhr.send();
 
@@ -14,23 +34,11 @@ xhr.send();
       var carModels = JSON.parse(xhr.responseText).models;
       //Get the select element
       var select = document.getElementById("model");
-      function removeOptions(selectElement) {
-          var i, L = selectElement.options.length - 1;
-          for(i = L; i >= 0; i--) {
-              selectElement.remove(i);
-          }
-        }
+      
       removeOptions(select);
 
-
-      //Loop through the car models and add them as options to the select element
-      for (var i = 0; i < carModels.length; i++) {
-        var option = document.createElement("option");
-        option.text = carModels[i];
-        
-        select.add(option);
-      }
-
+      add_new_options(carModels,select)
+      
     } else {
       console.log("Error retrieving car models");
     }
@@ -51,22 +59,10 @@ e.addEventListener("change", function() {
       var carModels = JSON.parse(xhr.responseText).models;
       //Get the select element
       var select = document.getElementById("model");
-      function removeOptions(selectElement) {
-          var i, L = selectElement.options.length - 1;
-          for(i = L; i >= 0; i--) {
-              selectElement.remove(i);
-          }
-        }
+
       removeOptions(select);
 
-
-      //Loop through the car models and add them as options to the select element
-      for (var i = 0; i < carModels.length; i++) {
-        var option = document.createElement("option");
-        option.text = carModels[i];
-        
-        select.add(option);
-      }
+      add_new_options(carModels,select)
 
     } else {
       console.log("Error retrieving car models");
