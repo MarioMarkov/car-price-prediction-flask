ea16597dc48b9202b1634d0f3e99d572f8f53264
MarioMarkov <mario11septemvri@gmail.com>
2023-05-07 13:34:20 +0200

    Abstracted operations from the app.py file to the helpers module ♻️

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import json
-import pickle
 import helpers
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from vertex_model import get_value_vertex_model
 from flask import jsonify
 from flask import Flask, request, render_template
 
@@ -24,7 +21,6 @@ with open("brands_models.json", "r") as file:
 
 brands  = list(brands_models.keys())
 
-
 @app.route('/')
 def home():
     return render_template('index.html', len = len(brands), brands = brands)
@@ -32,97 +28,33 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    
-    #pickled_model = pickle.load(open('pickles/xgb_model.pkl', 'rb'))
-    # With joblib it does not work 
     # Important to install scikit-learn v 1.2.1
-    tf_model = tf.keras.models.load_model('./saved_model/my_model')
 
-    pickled_model = pickle.load(open('pickles/final_model_pickle.pkl', 'rb'))
+    # Extract features from the request object 
+    car = helpers.extract_features(request)
 
+    # Perform basic preprocessing on the car object 
+    car = helpers.basic_preprocessing(car)
 
-    brand = str(request.form['brand'])
-    model = str(request.form['model'])
-    year = int(request.form['year'])
-    fuel = str(request.form['fuel'])
-    kms = float(request.form['kms'])
-    transmission = float(request.form['transmission'])
-    door_2 = float(request.form['2door'])
-    color = str(request.form['color'])
-    type_car = str(request.form['type'])
-    displacement = float(request.form['displacement'])
-    hp = float(request.form['hp'])
-    euro = float(request.form['euro'])
-    #ml_model = str(request.form['ml_model_hidden'])
-
-    car = pd.DataFrame({"brand": [brand],"model":[model] , "year": [year],"fuel": [fuel], "kms":[kms], 
-    'transmission':  [transmission],'2door': [door_2],'color':[color],'type':[type_car],'displacement':[displacement],'hp':[hp],'euro':[euro]
-    })
-    
-    car_vertex = {
-        "instances": [
-            { 
-            "_2door": door_2,"brand": brand,"color": color,"displacement": int(displacement),
-            "fuel": fuel,"hp": int(hp),"kms": kms,"model": model, "transmission": transmission,
-            "type": type_car,"year": str(year)
-            }
-        ]
-    }
-    print(car_vertex)
-    
-    car = car.drop(['euro'],axis='columns')
-    car["brand"] = car['brand'].astype(str) +"-"+ car["model"]
-
-    car = car.drop(['model'],axis='columns')
-
-    premium_brands = ["Porsche", "Audi","Mercedes-Benz","BMW"]
-
-    car['premium'] = np.where(car['brand'].isin(premium_brands), 1, 0)
-    
-    car = helpers.make_buckets(car)
-
-    car['new'] = np.where(car['year']>2018, 1, 0)
-
+    # Create a copy of the preprocessed car object to use with the TensorFlow model
     car_tf = car.copy()
 
-    # Flag for new cars only for decission trees
-    
-    # Ordinal Encoding
-    ordinal_enc_cols = ['brand','color']
-    one_hot_columns = ['fuel','type']  
-    
-    ordinal_encoder = pickle.load(open('pickles/ordinal_encoder', 'rb'))
-    oh_encoder = pickle.load(open('pickles/onehot_encoder', 'rb'))
-    
-    car = helpers.encode(car, 
-                    oh_encoder=oh_encoder,
-                    ordinal_encoder=ordinal_encoder,
-                    ordinal_columns=ordinal_enc_cols,
-                    oh_columns=one_hot_columns)
-    
-    car_tf = helpers.encode(car_tf, 
-                    oh_encoder=oh_encoder,
-                    ordinal_encoder=ordinal_encoder,
-                    ordinal_columns=ordinal_enc_cols,
-                    oh_columns=one_hot_columns)
-    
+    # Perform decision tree model-specific preprocessing on the car object 
+    car = helpers.dt_model_preprocessing(car)
 
-    prediction = pickled_model.predict(car)  # features Must be in the form [[a, b]]
-    prediction_tf = tf_model.predict(car_tf)
+    # Use the decision tree model to make a prediction
+    pred_dt = helpers.dt_model_prediction(car)
 
+    # Use the TensorFlow model to make a prediction 
+    pred_tf = helpers.tf_model_prediction(car_tf)
 
-    
-
-    output = int(prediction[0])
-    output_tf = int(prediction_tf[0])
-    output_vertex = int(get_value_vertex_model(car_vertex))
 
     return render_template(
         'index.html', 
         len = len(brands),
         brands = brands, 
-        prediction_text='Price  is '+ str(output)+ 'and tf_model' +str(output_tf)+ ' Vertex: '+
-        str(output_vertex))
+        prediction_text='DT Price is '+ str(pred_dt)+ ' Tensorflow: ' + str(pred_tf))
+
 
 @app.route("/get_brands/<brand>", methods=["GET"])
 def get_brands(brand):
--- helpers.py
+++ helpers.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import pickle
+import tensorflow as tf
+from requests import Request
 
 def encode(data,ordinal_encoder, oh_encoder, oh_columns, ordinal_columns):
     data[ordinal_columns] = ordinal_encoder.transform(data[ordinal_columns])
@@ -27,3 +31,72 @@ def make_buckets(data):
                      include_lowest = True)
   data["kms"] = data["kms"].astype("int32")
   return data
+
+def basic_preprocessing(car: pd.DataFrame) -> pd.DataFrame:
+  car = car.drop(['euro'],axis='columns')
+  premium_brands = ["Porsche", "Audi","Mercedes-Benz","BMW"]
+  car['premium'] = np.where(car['brand'].isin(premium_brands), 1, 0)
+  car = make_buckets(car)
+  return car
+   
+def dt_model_preprocessing(car :pd.DataFrame) -> pd.DataFrame:
+  car["brand"] = car['brand'].astype(str) +"-"+ car["model"]
+  car = car.drop(['model'],axis='columns')
+
+
+  car['new'] = np.where(car['year']>2018, 1, 0)
+
+  # Ordinal Encoding
+  ordinal_enc_cols = ['brand','color']
+  one_hot_columns = ['fuel','type']  
+    
+  ordinal_encoder = pickle.load(open('pickles/ordinal_encoder', 'rb'))
+  oh_encoder = pickle.load(open('pickles/onehot_encoder', 'rb'))
+    
+  car = encode(car, 
+                    oh_encoder=oh_encoder,
+                    ordinal_encoder=ordinal_encoder,
+                    ordinal_columns=ordinal_enc_cols,
+                    oh_columns=one_hot_columns)
+  return car
+
+def dt_model_prediction(car: pd.DataFrame) -> int:
+  pickled_model = pickle.load(open('pickles/final_model_pickle.pkl', 'rb'))
+  prediction = pickled_model.predict(car)  # features Must be in the form [[a, b]]
+  prediction = int(prediction[0])
+
+  return prediction
+
+
+def tf_model_prediction(car: pd.DataFrame) -> int:
+  tf_model = tf.keras.models.load_model('./saved_model/my_model')
+
+  input_dict = {name: tf.convert_to_tensor([value]) for name, value in car.items()}
+  prediction = tf_model.predict(input_dict)
+  prediction = int(prediction[0])
+
+  return prediction
+
+def extract_features(request : Request)-> pd.DataFrame:
+
+  brand = str(request.form['brand'])
+  model = str(request.form['model'])
+  year = int(request.form['year'])
+  fuel = str(request.form['fuel'])
+  kms = float(request.form['kms'])
+  transmission = float(request.form['transmission'])
+  door_2 = float(request.form['2door'])
+  color = str(request.form['color'])
+  type_car = str(request.form['type'])
+  displacement = float(request.form['displacement'])
+  hp = float(request.form['hp'])
+  euro = float(request.form['euro'])
+
+  car = pd.DataFrame({"brand": [brand],"model":[model] , "year": [year],"fuel": [fuel], "kms":[kms], 
+    'transmission':  [transmission],'2door': [door_2],'color':[color],'type':[type_car],'displacement':[displacement],'hp':[hp],'euro':[euro]
+  })
+  return car
+
+
+
+
