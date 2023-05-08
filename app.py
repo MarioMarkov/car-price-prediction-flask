8d069422cb794e25b421cbe52519ff05aa225b27
MarioMarkov <mario11septemvri@gmail.com>
2023-05-08 23:41:23 +0200

    Host no template

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def get_brands(brand):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, port=port, host='0.0.0.0')
